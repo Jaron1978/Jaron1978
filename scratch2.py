476d69609dc95d51f7e65ff3ab6e3cdfa8a72076
Ron Jackson <ron.jackson@amobee.com>
2022-01-05 21:15:26 +0000

    commit

---
+++ scratch2.py
@@ -0,0 +1,14 @@
+# Using end. 
+# print("Here", end=" ")
+# print("it is.......")
+
+# The code below demonstartes using the Escape sequences. 
+
+print("\t\t\tGame Credits!")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tRon Jackson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\ \\")
+print("\nSpecial thanks goes out to:")
+print("My wife, Jane. ")
+print("\n\nPress the Enter Key to continue. ")
