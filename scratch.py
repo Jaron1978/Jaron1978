5f217b6c7f2f9792e4d7332cbef263a9bd44bd11
Ron Jackson <ron.jackson@amobee.com>
2022-01-05 19:13:37 +0000

    commit

--- scratch.py
+++ scratch.py
@@ -10,6 +10,7 @@
 # ---------------------------------- Chapter 2 of Python for Beginners ---------------------------
 
 #Game Over Version 2, Demonstrates the use of quotes in Strings. 
+
 print("Program 'Game Over' 2.0")
 print("Same", "message", "as before")
 print("Just "
