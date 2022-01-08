67b0f7f40938c4258a31661b47ce8195216a6e12
Ron Jackson <ron.jackson@amobee.com>
2022-01-08 11:45:06 +0000

    Updated comments

--- if statement.py
+++ if statement.py
@@ -14,4 +14,15 @@ if password == "secret":
 input("\n\nPress the Enter Key to exit")
 
 # The password is secret. If the User enters secret, they get Access Granted. Any other password, and they are prompted to hit Enter to continute. 
+# All if statements have a condition, a condition of either True or False. 
+# The difference between line 9 and 11. 
+# Line 9: = is a is an assignment statement, 
+# Line 11: == is a condition, evaluates true or false. 
+
+# = equal to, 
+# != Not equal to, 
+# > greater than, 
+# < less than, 
+# >= Greater than or equal to
+# <= less than or equal to. 
 
