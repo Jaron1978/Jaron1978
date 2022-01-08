903547a7b708037bf9af1a9a47eef7b4e8253d3d
Ron Jackson <ron.jackson@amobee.com>
2022-01-08 11:29:13 +0000

    use of the if statement in a simple password format

---
+++ if statement.py
@@ -0,0 +1,17 @@
+# Branching - Making a decision to take one path or another. 
+# Using the if statement, programs can branch to different sections of code, or just skip it depending on how things have been setup. 
+
+# Password - This demonstrates use of the if statement. 
+
+print("Welcome to System Security Inc")
+print("--Security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+
+input("\n\nPress the Enter Key to exit")
+
+# The password is secret. If the User enters secret, they get Access Granted. Any other password, and they are prompted to hit Enter to continute. 
+
