812297a51f3975ff409a2bb986ac8cb0f9de8133
Ron Jackson <ron.jackson@amobee.com>
2022-01-08 12:01:27 +0000

    commit

---
+++ Indentation.py
@@ -0,0 +1,19 @@
+# By indenting lines, we ceate blocks (See line 11 and 12)
+
+name = "Ron"
+
+print("Welcome to System Security Inc")
+print("--Security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+    print("Welcome", name , "You must be very improtant"
+    )
+
+input("\n\nPress the Enter Key to exit")
+
+# As you can see, lines 11 and 12 are indented to create a Block. 
+# I have also added the name variable so that when the code runs, it runs with my name. 
+# Alternativley, I could use the input function to have a user enter their name but its my code so its my name. 
