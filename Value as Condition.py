9301cf2457a6909045fb28144d89cbdd2f309959
Ron Jackson <ron.jackson@amobee.com>
2022-01-09 14:02:46 +0000

    value (money) treated as a condition

---
+++ Value as Condition.py
@@ -0,0 +1,13 @@
+# Treating a Value as a Condition
+
+print("Welcome to Food Chateau")
+print("It would seem that we are  very busy this evening!!")
+
+money = int(input("How much would you like to slip the Maitre D'? "))
+
+if money:
+    print("Ah! I am reminded of a table. Please follow me!")
+else:
+    print("Please sit and wait. This may take a while!!")
+
+input("\n\nPress Enter to exit")
