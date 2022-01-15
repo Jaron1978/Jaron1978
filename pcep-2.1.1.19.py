cdd4796ed998915e70cc778e6729d50712e9f0b4
Ron Jackson <ron.jackson@amobee.com>
2022-01-15 14:43:13 +0000

    Added in /n as well as "sting"*2

---
+++ pcep-2.1.1.19.py
@@ -0,0 +1,11 @@
+# Objective is to play with the code and get an idea of what happens. 
+# Add in \n as well as double a string using the *2
+
+print("    *\n")
+print(" *    *  \n"*2)
+print("  *   *\n")
+print(" *     *\n")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
