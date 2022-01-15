e755114969bd4bb0da2605d5558711bf6a85755c
Ron Jackson <ron.jackson@amobee.com>
2022-01-15 13:56:22 +0000

    Using sep"***" and end"..."

---
+++ pcep-2.1.1.18.py
@@ -0,0 +1,5 @@
+#  This lab is about becoming familier with the print() function and its formatting capabilities. 
+# The objection is to modify the the line of code to read Programming***Essentials***in...Python using the sep and end keywords. 
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
