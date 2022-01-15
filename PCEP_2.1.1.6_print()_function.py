0c7d62bf6173c41f5255e1373016185f60ea7ca1
Ron Jackson <ron.jackson@amobee.com>
2022-01-15 13:35:21 +0000

    print() function

---
+++ PCEP_2.1.1.6_print()_function.py
@@ -0,0 +1,7 @@
+# The first lab of the Python PCEP Course. 
+# 2.1.1.6 Lab - The Print() function
+
+print("Hello World")
+print() # blank line
+print("blank space about using an empty print function.")
+
