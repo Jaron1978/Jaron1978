c956c1fe642a5ced10a31fdee4db92b3ac00a108
Ron Jackson <ron.jackson@amobee.com>
2022-01-15 14:57:01 +0000

    Completed using the '' and \n line break

---
+++ pcep-2.2.1.11.py
@@ -0,0 +1,5 @@
+# Write a one-line piece of code, using the print() function, as well as the newline and escape characters, to match the expected result outputted on three lines below.
+#"I'm"
+#""learning""
+#"""python"""
+print('"I\'m"\n' '""learning""\n' '"""python"""' )
