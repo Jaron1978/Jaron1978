0d0b7aa93cf87cfabed2e6e4498491b749efc834
Ron Jackson <ron.jackson@amobee.com>
2022-01-16 14:25:58 +0000

    Mathmatics and Variables

---
+++ pcep-2.4.1.7.py
@@ -0,0 +1,19 @@
+# Create three variables and assign values to said variables. 
+# Create a new variable called total_apples and equal to addition of the three variables above, 
+# print the value stored in total_apples. 
+
+from re import X
+
+
+john = 3
+mary = 5
+adam = 6
+
+total_apples = john / mary / adam
+
+print(total_apples)
+
+# + answer is 14, 
+# * answer is 90
+#  // answer is 0 (Should be 05. but // is an integer)
+# / is 0.9 (Floating point)
