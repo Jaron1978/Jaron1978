7ab44455dfe35c7ce686234c364ec37a9160799d
Ron Jackson <ron.jackson@amobee.com>
2022-01-16 15:14:14 +0000

    evaluating simple arithmatic

---
+++ pcep-2.6.1.9.py
@@ -0,0 +1,7 @@
+a = float(input("Enter a value for a: ")) # Prompts the user to enter a value for a
+b = float(input("Enter a value for b: ")) # Prompts the user to enter a value for b
+
+# print(a + b)  Prints the value of a + b
+# print(a -50 b) Print the value of a - b
+# print(a * b) Print the value of a * b
+# print(a / b) Note the single /, this is because we are using floats. If we were using Integers (Whole Numbers), we would use //
